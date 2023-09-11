c76d0067408d3334f9006a259a1489b25b91f210
Michal <m@wp.pl>
2023-09-11 22:43:09 +0200

    First version of Flappy Bird. Added player, walls and movement.

---
+++ assets/scripts/Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+const UP = Vector2(0,-1)
+const FLAP = 200
+const MAXFALLSPEED = 400
+const MAX_ROTATION_SPEED = 20
+const GRAVITY = 10
+const MAX_ROTATION_DOWNWARDS = 50
+const DEGREE_BY_WHICH_TO_ROTATE = 2
+
+var motion = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+	if rotation_degrees > MAX_ROTATION_DOWNWARDS:
+		rotation_degrees = MAX_ROTATION_DOWNWARDS
+		
+	if Input.is_action_just_pressed("FLAP"):
+		motion.y = -FLAP
+		rotation_degrees = -(FLAP / 5)
+	else:
+		rotation_degrees += DEGREE_BY_WHICH_TO_ROTATE
+		
+
+	motion = move_and_slide(motion, UP)
+
+
---
+++ assets/scripts/Walls.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _physics_process(delta):
+	
+	position += Vector2(-2,0) 
+	
